8593a85c648904583373afa1c97f64a848112fc3
Oliver Stone <oliverstone001@gmail.com>
2025-12-04 18:56:58 +0000

    Complete V2 of my port scanner

--- ScannerV2.py
+++ ScannerV2.py
@@ -90,78 +90,36 @@ def rangeScan():
 # Defining specific scan function
 def specificScan():
 
-    # Ask user for the IP address while validating input including checking if the IP address is valid or not
-    while True:
+    # Get IP address while validating input including checking if the IP address is valid or not
+    print(
+        "\nEnter the IP address:"
+    )
+    address = getIP()
+
+    # Get port number
+    print(
+        "\n"
+        "Enter port number:"
+    )
+    port = getPort()
+    
+
+    # check the port and print if the port is open or closed
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.5)
+    result = sock.connect_ex((f"{address}", port))
+    sock.close()
+
+    if result == 0:
         print(
-            "\n"
-            "Enter IP address:"
+             "Port " + str(port) + " is open.\n"
         )
-        address = input("= ")
+    
+    else:
+        print(
+            "Port " + str(port) + " is closed.\n"
+        )
 
-        # Check if the IP address is a valid IP address
-        if getIP() == True:
-
-            # Ask the user for the port number while validating input
-            while True:
-                print(
-                    "\n"
-                    "Enter the port number:"
-                )
-                try:
-                    port = int(input("= "))
-
-                    # Check the port is a valid port
-                    if port >=0 and port <=65535:
-
-                        # Get time of results
-                        date = datetime.datetime.now()
-
-                        # Print scan summary
-                        print(
-                            "\n"
-                            "Date: " + str(date) + "\n"
-                            "IP Address: " + address + "\n"
-                            "Port: " + str(port) + "\n"
-                        )
-
-                        # check the port and print if the port is open or closed
-                        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        sock.settimeout(0.5)
-                        result = sock.connect_ex((f"{address}", port))
-                        sock.close()
-
-                        if result == 0:
-                            print(
-                                "Port " + str(port) + " is open.\n"
-                            )
-                            break
-                        else:
-                            print(
-                                "Port " + str(port) + " is closed.\n"
-                            )
-                            break
-
-                    # If the user enters an invalid port    
-                    else:
-                        print(
-                            "\n"
-                            "Invalid port\n"
-                        )
-                
-                # If the user enters an invalid port
-                except ValueError:
-                    print(
-                        "\n"
-                        "Invalid port\n"
-                    )
-
-            break
-        # If user enters an invalid IP address            
-        else:
-            print(
-                "\n"
-                "Invalid IP"
-            )
 
 def scanSinglePort(address, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -189,10 +147,12 @@ def getPort():
         try:
             port = int(input("= "))
 
-            if port >= 0 and port <= 65534:
+            if port >= 0 and port <= 65535:
                 return port
             else:
-                "\nInvalid port, try again"
+                print(
+                    "\nInvalid port, try again"
+                )
         except ValueError:
             print(
                 "\nInvalid port, try again"
@@ -212,7 +172,7 @@ def getThreads():
         try:
             threads = int(input("= "))
 
-            if threads >= 1 or threads <= 200:
+            if threads >= 1 and threads <= 200:
                 return threads
             else:
                 print(
@@ -225,3 +185,14 @@ def getThreads():
 
 main()
 
+
+# Get time of results
+#    date = datetime.datetime.now()
+
+# Print scan summary
+#print(
+#"\n"
+#"Date: " + str(date) + "\n"
+#"IP Address: " + address + "\n"
+#"Port: " + str(port) + "\n"
+#)
